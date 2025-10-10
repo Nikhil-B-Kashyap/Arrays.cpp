9605ea5173ca9072347176cde4f52e46b2f834d0
Nikhil-B-Kashyap <nikhilbkashyap55@gmail.com>
2025-10-10 20:25:37 +0530

    Add files via upload

---
+++ Valid Parentheses.cpp
@@ -0,0 +1,52 @@
+/*Valid Parentheses
+Solved 
+You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+Return true if s is a valid string, and false otherwise.
+
+Example 1:
+
+Input: s = "[]"
+
+Output: true
+Example 2:
+
+Input: s = "([{}])"
+
+Output: true
+Example 3:
+
+Input: s = "[(])"
+
+Output: false*/
+
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        unordered_map<char, char> closeToOpen = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        for (char c : s) {
+            if (closeToOpen.count(c)) {
+                if (!stack.empty() && stack.top() == closeToOpen[c]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.empty();
+    }
+};
