811eaff578e654de25fedf58aa6c33d68c5f31ca
Nikhil-B-Kashyap <nikhilbkashyap55@gmail.com>
2025-10-11 20:33:51 +0530

    Add files via upload

---
+++ Minimum Stack.cpp
@@ -0,0 +1,69 @@
+/*Minimum Stack
+Solved 
+Design a stack class that supports the push, pop, top, and getMin operations.
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+Each function should run in 
+O
+(
+1
+)
+O(1) time.
+
+Example 1:
+
+Input: ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
+
+Output: [null,null,null,null,0,null,2,1]
+
+Explanation:
+MinStack minStack = new MinStack();
+minStack.push(1);
+minStack.push(2);
+minStack.push(0);
+minStack.getMin(); // return 0
+minStack.pop();
+minStack.top();    // return 2
+minStack.getMin(); // return 1
+
+*/
+
+
+class MinStack {
+public:
+        stack<int> s;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        s.push(val);
+    }
+    
+    void pop() {
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        stack<int> a;
+        int miN = s.top();
+        while(s.size()){
+            miN = min(miN, s.top());
+            a.push(s.top());
+            s.pop();
+        }
+        while(a.size()){
+            s.push(a.top());
+            a.pop();
+        }
+        return miN;
+    }
+};
